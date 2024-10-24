eda036bfbce70cec84bcec2b5ae4b101bd2c7e73
hajaemin <woals3527@naver.com>
2024-10-24 14:00:15 +0900

    first commit

---
+++ src/main/java/org/zerock/docker1/controller/SampleController.java
@@ -0,0 +1,29 @@
+package org.zerock.docker1.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+public class SampleController {
+
+    private final DataSource dataSource;
+
+    @GetMapping("hello")
+    public String[] hello() {
+
+        log.info("hello");
+        log.info("===============");
+
+        log.info(dataSource);
+
+        log.info("===============");
+
+        return new String[]{"Ha", "Jaemin", "hjm", dataSource.toString()};
+    }
+}
